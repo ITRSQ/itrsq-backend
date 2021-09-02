307279f368718b8ccfff428cf42b682fcd39a47e
Julian Tran <juliantran003@gmail.com>
2021-09-02 12:28:04 +0200

    added get route for testimonials

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -2,6 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Testimonial = require("../models/Testimonial");
 
+router.get(`/testimonials`, async (req, res) => {
+  console.log("Using Route : /testimonials");
+  console.log(req.query);
+  try {
+    const testimonials = await Testimonial.find();
+    res.status(200).json(testimonials);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 router.post("/testimonial/create", async (req, res) => {
   console.log("Using Route : /testimonial/create");
   try {
