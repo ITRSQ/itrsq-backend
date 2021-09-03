56a46539bac3ee6771a7ea41afe0c0aab5f7cfb0
Julian Tran <juliantran003@gmail.com>
2021-09-03 10:39:16 +0200

    email verification works (both verification abd format validation)

--- routes/mails.js
+++ routes/mails.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const languages = require("../lang/errorMessages.json");
+const axios = require("axios");
 
 const API_KEY = process.env.MAILGUN_API_KEY;
 const DOMAIN = process.env.MAILGUN_DOMAIN;
 const mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
 
 router.post(`/mail/contact`, async (req, res) => {
-  const isValidMail = (mail) => {
-    const mailformat =
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    return mail.match(mailformat) ? true : false;
-  };
   console.log("Using Route : /mail/contact");
   const { from, subject, orderRef, firstName, lastName, text } = req.fields;
 
@@ -22,31 +18,37 @@ router.post(`/mail/contact`, async (req, res) => {
     lastName !== undefined ||
     text !== undefined
   ) {
-    if (!isValidMail(from)) {
-      return res.status(400).json({
-        error: languages.en.invalidEmail,
-      });
-    } else {
-      try {
-      } catch (error) {}
-      try {
-        const data = {
-          from: `${firstName} ${lastName} <${from}>`,
-          to: "ITRSQ <support@itrsq.com>",
-          subject: `${orderRef && orderRef}  ${subject}`,
-          text: text,
-        };
-        await mailgun.messages().send(data, (error, body) => {
-          if (body.message === "Queued. Thank you.") {
-            res.status(200).json("Email sent!");
-            console.log(body);
-          } else {
-            res.status(400).json({ error: error.message });
-          }
-        });
-      } catch (error) {
-        res.status(400).json({ error: error.message });
+    try {
+      const response = await axios.get(
+        `https://emailvalidation.abstractapi.com/v1/?api_key=${process.env.ABSTRACT_API_KEY}&email=${from}`
+      );
+      if (
+        response.data.deliverability === "DELIVERABLE" &&
+        response.data.is_valid_format
+      ) {
+        try {
+          const data = {
+            from: `${firstName} ${lastName} <${from}>`,
+            to: "ITRSQ <support@itrsq.com>",
+            subject: `${orderRef && orderRef}  ${subject}`,
+            text: text,
+          };
+          await mailgun.messages().send(data, (error, body) => {
+            if (body.message === "Queued. Thank you.") {
+              res.status(200).json("Email sent!");
+              console.log(body);
+            } else {
+              res.status(400).json({ error: error.message });
+            }
+          });
+        } catch (error) {
+          res.status(400).json({ error: error.message });
+        }
+      } else {
+        res.status(400).json({ error: languages.en.invalidEmail });
       }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
   } else {
     return res.status(400).json({
