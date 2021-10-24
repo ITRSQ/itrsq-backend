989fc1a7579e62f8ca2cecd43cf9ed75d9ee7625
Julian Tran <juliantran003@gmail.com>
2021-10-24 16:20:24 +0100

    cloudinary added to articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Article = require("../models/Article");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 router.get(`/articles`, async (req, res) => {
   console.log("Using Route : /articles");
@@ -16,12 +23,18 @@ router.get(`/articles`, async (req, res) => {
 router.post("/article/create", async (req, res) => {
   console.log("Using Route : /article/create");
   try {
-    const { author, text, picture, title, tags } = req.fields;
+    let newPicture = "";
+    const { author, text, title, tags } = req.fields;
+    const picture = req.files.picture.path;
+    const result = await cloudinary.uploader.upload(picture, {
+      folder: "/articles",
+    });
+    newPicture = result.url;
 
     const newArticle = await new Article({
       author,
       text,
-      picture,
+      picture: newPicture,
       title,
       tags,
     });
